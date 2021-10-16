44efc1e79526765072af0debca98584d0eeb6b64
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-10-16 16:41:25 +0800

    Create main.h

---
+++ week3/ch02/main.h
@@ -0,0 +1,23 @@
+/***************.h文件*******************/
+
+#ifdef _LeftHeap_H
+
+/* Minimal set of priority queue operations */
+/* Note that nodes will be shared among several */
+/* leftist heaps after a merge;the user must */
+/* make sure to not use the old leftist heaps */
+
+PriorityQueue Initialize(void);
+int IsEmpty(H);
+ElementType FindMin(PriorityQueue H);
+int IsEmpty(PriorityQueue H);
+PriorityQueue Merge(PriorityQueue H1,PriorityQueue H2);
+void Print(PriorityQueue H);
+
+#define Insert(X,H)  (H=Insert1((X),H))
+#define DeleteMin(H)  (H=DeleteMin1(H))
+
+PriorityQueue Insert1(ElementType X,PriorityQueue H);
+PriorityQueue DeleteMin1(PriorityQueue H);
+
+#endif
