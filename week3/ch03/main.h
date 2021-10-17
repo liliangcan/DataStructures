3bd3ad06ad49c8741988a32d3f9cd121eba43386
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-10-17 10:55:01 +0800

    Create main.h

---
+++ week3/ch03/main.h
@@ -0,0 +1,11 @@
+#ifdef _BinQueue_H
+
+int IsEmpty(BinQueue H);
+BinQueue Initialize(void);
+BinTree CombineTrees(BinTree T1,BinTree T2);
+BinQueue Merge(BinQueue H1,BinQueue H2);
+ElementType DeleteMin(BinQueue H);
+void Insert(ElementType X,H);
+void Print(BinQueue H);
+
+#endif
