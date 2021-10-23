522b3ae1e28c5dc7cdf70df72ffec543a089b460
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-10-23 23:34:21 +0800

    Create main.c

---
+++ week4/ch01/main.c
@@ -0,0 +1,102 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+typedef int ElementType;
+#define Size 15
+#define Cutoff 3
+
+void FatalError(char *s)
+{
+    printf("%s\n",s);
+}
+
+void InsertionSort(ElementType A[],int N)
+{
+	int j,P;
+
+	ElementType Tmp;
+	for(P=1;P<N;P++)
+	{
+		Tmp=A[P];
+		for(j=P;j>0 && A[j-1]>Tmp;j--)
+			A[j]=A[j-1];
+		A[j]=Tmp;
+	}
+}
+
+void Swap(ElementType *a,ElementType *b)
+{
+	ElementType Tmp;
+	Tmp=*a;
+	*a=*b;
+	*b=Tmp;
+}
+
+void Merge(ElementType A[],ElementType TmpArray[],int Lpos,int Rpos,int RightEnd)
+{
+	int i,LeftEnd,NumElements,TmpPos;
+
+	LeftEnd=Rpos-1;           //曾错误打成Lpos-1导致运行有问题
+	TmpPos=Lpos;
+	NumElements=RightEnd-Lpos+1;
+
+	while(Lpos<=LeftEnd && Rpos<=RightEnd)	// main loop
+		if(A[Lpos]<=A[Rpos])
+			TmpArray[TmpPos++]=A[Lpos++];
+		else
+			TmpArray[TmpPos++]=A[Rpos++];
+
+	while(Lpos<=LeftEnd)
+		TmpArray[TmpPos++]=A[Lpos++];
+	while(Rpos<=RightEnd)
+		TmpArray[TmpPos++]=A[Rpos++];
+
+	for(i=0;i<NumElements;i++,RightEnd--)	// cpoy tmparray back
+		A[RightEnd]=TmpArray[RightEnd];
+}
+
+void MSort(ElementType A[],ElementType TmpArray[],int Left,int Right)
+{
+	int Center;
+
+	if(Left<Right)
+	{
+		Center=(Left+Right)/2;
+		MSort(A,TmpArray,Left,Center);
+		MSort(A,TmpArray,Center+1,Right);
+		Merge(A,TmpArray,Left,Center+1,Right);
+	}
+}
+
+void MergeSort(ElementType A[],int N)
+{
+	ElementType *TmpArray;
+
+	TmpArray=malloc(N * sizeof(ElementType));
+	if(TmpArray!=NULL)
+	{
+		MSort(A,TmpArray,0,N-1);
+		free(TmpArray);
+	}
+	else
+		FatalError("No space for tmp array!!!");
+}
+
+int main()
+{
+	int i,k;
+	ElementType A[Size];
+
+	srand((unsigned int)time(NULL));
+	for(i=0;i<Size;i++)
+		A[i]=rand()%100+1;
+	printf("未排序时：");
+	Print(A,Size);
+
+	MergeSort(A,Size);
+	printf("使用归并排序结果为：");
+
+	Print(A,Size);	
+	return 0;
+}
