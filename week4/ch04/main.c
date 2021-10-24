1a341b9fb204d612177387a4f8f95d1768c1294f
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-10-24 13:57:24 +0800

    Update main.c

--- week4/ch04/main.c
+++ week4/ch04/main.c
@@ -20,40 +20,40 @@ void BucketSort(int keys[],int size,int BucketSize)
 	int i,j,index;
 	KeyNode p,k;
 	KeyNode node;
-	KeyNode *BucketTable=malloc(BucketSize*sizeof(KeyNode*));
+	KeyNode *BucketTable=malloc(BucketSize * sizeof(KeyNode));
 	
 	k = NULL;
 	for(i=0;i<BucketSize;i++)
 	{
 		BucketTable[i]=malloc(sizeof(KeyNode));
-		BucketTable[i]->key=0;
+		BucketTable[i]->key=0;                 //桶的第一个节点中key表示桶内数据量，初始值为0
 		BucketTable[i]->next=NULL;
 	}
 	for(j=0;j<size;j++)
 	{
-		node=malloc(sizeof(KeyNode));
+		node=malloc(sizeof(KeyNode));        //为节点分配空间
 		node->key=keys[j];
 		node->next=NULL;
-		index=keys[j]/10;
+		index=keys[j]/10;                 //分桶
 		p=BucketTable[index];
-		if(p->key==0)
+		if(p->key==0)                            //若为空桶，直接往后接
 		{
 			BucketTable[index]->next = node;
 			(BucketTable[index]->key)++;
 		}
 		else
 		{
-			while(p->next!=NULL && p->next->key<=node->key)
-				p = p->next;
-			node->next=p->next;
+			while(p->next!=NULL && p->next->key <= node->key)    //若不为空桶，类比链表插入
+				p = p->next;               
+			node->next=p->next;                 //插入节点
 			p->next=node;
-			(BucketTable[index]->key)++;
+			(BucketTable[index]->key)++;           //记录数据量加一
 		}
 	}
 
 	printf("桶排序结果为：");
-	for(i=0;i<BucketSize;i++)
-		for(k=BucketTable[i]->next;k!=NULL;k=k->next)
+	for(i=0;i<BucketSize;i++)                   //外循环每个桶
+		for(k=BucketTable[i]->next;k!=NULL;k=k->next)     //内循环桶内依次输出
 			printf("%d ",k->key);
 	printf("\n");
 }
