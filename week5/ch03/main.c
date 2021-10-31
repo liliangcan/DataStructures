bbe2988ccf481e8cbf2ebb413902c373593c3fd5
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-10-31 12:19:57 +0800

    Create main.c

--- week5/ch03/main.c
+++ week5/ch03/main.c
@@ -1,3 +1,5 @@
+//上面的这个代码无法成功运行
+/*
 #include<stdio.h>
 
 static unsigned long Seed=1;
@@ -36,5 +38,39 @@ int main()
 	for(i=0;i<10;i++)
 		printf("%d ",Array[i]);
   
+	return 0;
+}*/
+
+//这是书上那个简单的例子（可实现）
+
+#include<stdio.h>
+
+static unsigned long Seed=1;
+
+#define A 7
+#define M 11
+
+int Random(void)
+{
+	Seed=(A*Seed)%M;
+	return (int)Seed;
+}
+
+void Initialize(unsigned long InitVal)
+{
+	Seed=InitVal;
+}
+
+int main()
+{
+	int i;
+	int Array[20];
+	for(i=0;i<20;i++)
+	{
+		Array[i]=Random();
+		Initialize((unsigned long)Array[i]);
+	}
+	for(i=0;i<20;i++)              //两轮重复的随机数
+		printf("%d ",Array[i]);
 	return 0;
 }
