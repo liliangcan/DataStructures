cde3205b0dc2befecda4b6209da5c7255b5143e5
liliangcan <91586917+liliangcan@users.noreply.github.com>
2021-11-07 14:41:39 +0800

    Create main.h

---
+++ week6/ch04/main.h
@@ -0,0 +1,9 @@
+#ifdef _SkipList_H
+
+Position Find(ElementType Item,SkipList L);
+SkipList Insert(ElementType Item,SkipList L);
+SkipList Initialize(void);
+SkipList Delete(ElementType Item,SkipList T);
+
+
+#endif /* _SkipList_H */
